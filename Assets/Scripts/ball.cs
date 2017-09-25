d5ff8c39feb0e4213b9a1ecb697e08d3e9f11793
Hansel Perez <hanselperez1@gmail.com>
2017-09-25 12:59:31 -0400

    Added global gravity and coliders to sides of screen

--- Assets/Scripts/ball.cs
+++ Assets/Scripts/ball.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Ball : MonoBehaviour {
 	public Paddle paddle; 
 
+	private bool hasStarted = false;
 	private Vector3 paddleToBallVector;
 
 	// Use this for initialization
@@ -14,6 +15,16 @@ public class Ball : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		this.transform.position = paddle.transform.position + paddleToBallVector;
+		if (!hasStarted) {
+			// Lock the ball relative to the paddle.
+			this.transform.position = paddle.transform.position + paddleToBallVector;
+
+			// Wait for a mouse press to launch.
+			if (Input.GetMouseButtonDown(0)) {
+				print ("Mouse clicked, launch ball");
+				hasStarted = true;
+				this.rigidbody2D.velocity = new Vector2 (2f, 10f);
+			}
+		}
 	}
 }
