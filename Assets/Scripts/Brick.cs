77b1a250530c3186e7af4c9f7f43e80753113a6d
Hansel Perez <hanselperez1@gmail.com>
2017-09-29 08:21:16 -0400

    Got issues, man. Can't navigate between levels...

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Brick : MonoBehaviour {
 
-	public int maxHits;
 	public Sprite[] hitSprites;
+
 	private int timesHit;
 	private LevelManager levelManager;
 
@@ -21,18 +21,28 @@ public class Brick : MonoBehaviour {
 	}
 
 	void OnCollisionEnter2D (Collision2D col) {
+		bool isBreakable = (this.tag == "Breakable");
+		if (isBreakable) {
+		HandleHits();
+		}
+
+	}
+
+	void HandleHits () {
 		timesHit++;
+		int maxHits = hitSprites.Length + 1;
 		if (timesHit >= maxHits) {
 			Destroy(gameObject);
 		} else {
 			LoadSprites();
 		}
-
 	}
 
 	void LoadSprites () {
 		int spriteIndex = timesHit - 1;
-		this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+		if (hitSprites[spriteIndex]) {
+			this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+		}
 	}
 
 	// TODO Remove thsi method once we can win.
