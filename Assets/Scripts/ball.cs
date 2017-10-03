60add656095f00c3a975194e1366986a0ed5564b
Hansel Perez <hanselperez1@gmail.com>
2017-10-03 13:00:53 -0400

    Adding sound effects to collisions.

--- Assets/Scripts/ball.cs
+++ Assets/Scripts/ball.cs
@@ -7,12 +7,14 @@ public class Ball : MonoBehaviour {
 	private bool hasStarted = false;
 	private Vector3 paddleToBallVector;
 
+
+
 	// Use this for initialization
 	void Start () {
+		//AudioSource audiottt = GetComponent<AudioSource>();
 		paddle = GameObject.FindObjectOfType<Paddle>();
 		paddleToBallVector = this.transform.position - paddle.transform.position;
-		print(paddleToBallVector.y);
-		AudioClip.play();
+		//audiottt.Play();
 	}
 	
 	// Update is called once per frame
@@ -26,7 +28,19 @@ public class Ball : MonoBehaviour {
 				print ("Mouse clicked, launch ball");
 				hasStarted = true;
 				this.GetComponent<Rigidbody2D>().velocity = new Vector2 (2f, 10f);
+
 			}
+
 		}
+	}
+
+	void OnCollisionEnter2D (Collision2D collision) {
+		AudioSource audiottt = GetComponent<AudioSource>();
+
+		if (hasStarted) {
+			audiottt.Play();
+		}
+
+		
 	}
 }
