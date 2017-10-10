fe980c2ef82785536f0483edebec3e8036b7ea9f
Hansel Perez <hanselperez1@gmail.com>
2017-10-10 12:54:52 -0400

    Finished game. Next step is to add custom artwork.

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -3,19 +3,37 @@ using System.Collections;
 
 public class Paddle : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public bool autoPlay = false;
+
+	private Ball ball;
+
+	void Start() {
+		ball = GameObject.FindObjectOfType<Ball>();
+		print(ball);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (autoPlay == false) {
+			MoveWithMouse();
+		} else {
+			AutoPlay();
+		}
+	}
+
+	void AutoPlay() {
 		Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y, 0f);
-
-		float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
-
-		paddlePos.x = Mathf.Clamp(mousePosInBlocks, .5f, 15.5f);
-
+		Vector3 ballPos = ball.transform.position;
+		float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;	
+		paddlePos.x = Mathf.Clamp(ballPos.x, .5f, 15.5f);		
 		this.transform.position = paddlePos;
 	}
+
+	void MoveWithMouse() {
+		Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y, 0f);	
+		float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;	
+		paddlePos.x = Mathf.Clamp(mousePosInBlocks, .5f, 15.5f);		
+		this.transform.position = paddlePos;
+	}
+
 }
