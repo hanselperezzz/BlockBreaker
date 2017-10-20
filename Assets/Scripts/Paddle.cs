360a9d9b7d1c08401f8aa841f13f3d3c264853d3
Hansel Perez <hanselperez1@gmail.com>
2017-10-20 12:39:21 -0400

    Error in first two levels.

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Paddle : MonoBehaviour {
 
 	public bool autoPlay = false;
+	public float minX, maxX;
 
 	private Ball ball;
 
@@ -25,14 +26,14 @@ public class Paddle : MonoBehaviour {
 		Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y, 0f);
 		Vector3 ballPos = ball.transform.position;
 		float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;	
-		paddlePos.x = Mathf.Clamp(ballPos.x, .5f, 15.5f);		
+		paddlePos.x = Mathf.Clamp(ballPos.x, minX, maxX);		
 		this.transform.position = paddlePos;
 	}
 
 	void MoveWithMouse() {
 		Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y, 0f);	
 		float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;	
-		paddlePos.x = Mathf.Clamp(mousePosInBlocks, .5f, 15.5f);		
+		paddlePos.x = Mathf.Clamp(mousePosInBlocks, minX, maxX);		
 		this.transform.position = paddlePos;
 	}
 
